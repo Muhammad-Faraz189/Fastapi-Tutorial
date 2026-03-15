ca7d4280fbd952ae66a71659dee4286fc56c8694
Muhammad Faraz <farazmaliknum37@gmail.com>
2026-03-15 22:27:13 +0500

    update

--- test_pydantic.py
+++ test_pydantic.py
@@ -1,16 +1,20 @@
 from pydantic import BaseModel
+from typing import List,Dict
 
 class Student(BaseModel):
     name: str
     age: int
     address: str
     weight:float
+    total_student_name:List[str]
+    contact_details:Dict[str,int]
 
 
 def student_info(student: Student):
     print(student.name)
     print(student.age)
     print(student.address)
+    print(student.contact_details)
     print("student info")
     print("===========================================")
 
@@ -22,7 +26,7 @@ def update_student_data(student:Student):
     print("update student data")
 
 
-s1 = {"name":"Faraz","age":25,"address":"Chak no 97 RB Johal Faisalabad","weight":67.7}  
+s1 = {"name":"Faraz","age":25,"address":"Chak no 97 RB Johal Faisalabad","weight":67.7,"total_student_name":["Ayan","Ali","Misbah"],"contact_details":{"Ayan":5463,"Ali":9807,"Misbah":3421}}  
 obj =Student(**s1)  
 
 # obj = Student(name="Faraz", age=25, address="Chak no 97 RB Johal Faisalabad") #we can also write like that.
